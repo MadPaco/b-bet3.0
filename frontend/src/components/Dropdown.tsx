97817a34434c74de4efa1bd4d4640c64a762aada
Alexander Bachmann <alexander-bachmann@gmx.de>
2024-03-09 21:09:27 +0100

    Prevent dropdown menu from overlapping navbar and close menu on outside click

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef } from 'react';
 
 interface DropdownProps {
     title: string;
@@ -6,26 +7,39 @@ interface DropdownProps {
     onToggle: () => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ title, items, isOpen, onToggle}) =>{
+const Dropdown: React.FC<DropdownProps> = ({ title, items, isOpen, onToggle }) => {
+    const node = useRef<HTMLDivElement>(null);
 
-    return(
-        <div className="text-gray-50">
-            <div>
-                <button onClick={onToggle}>{title}</button>
-            </div>
-            <div>
-            { isOpen && (
-                    <div className="origin-top-right absolute right-0 mt-2 rounded-md 
-                    shadow-lg bg-gray-500 ring-1 ring-black ring-opacity-5
-                    animate-fade-down animate-once animate-ease-linear">
-                        <div className="mx-3 flex flex-col text-right">
-                            {items.map((item, key) =>(
-                                <a className="py-1" key={key} href="#">{item}</a>
-                            ))}
-                        </div>
+    const handleClickOutside = (e: MouseEvent) => {
+        if (node.current && !node.current.contains(e.target as Node)) {
+            onToggle();
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen]);
+
+    return (
+        <div className="text-gray-50 relative" ref={node}>
+            <button onClick={onToggle}>{title}</button>
+            {isOpen && (
+                <div className="mt-1 origin-top-right absolute top-full rounded-md shadow-lg bg-gray-500 ring-1 ring-black ring-opacity-5 animate-fade-down animate-once animate-duration-150 animate-ease-linear">
+                    <div className="mx-3 flex flex-col text-right">
+                        {items.map((item, key) => (
+                            <a className="py-1" key={key} href="#">{item}</a>
+                        ))}
                     </div>
-                )}
-            </div>
+                </div>
+            )}
         </div>
     );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 const Header: React.FC = () =>{
     return(
-        <header className="bg-black flex p-2 justify-between items-center">
+        <header className="bg-black flex justify-between items-center px-2">
             <h1 className="text-2xl font-bold text-gray-500">B-Bet</h1>
             <Navigation />
         </header>
