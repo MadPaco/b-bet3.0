eb502f7f88fbed02d139c3e94441580ac07870d8
Alexander Bachmann <alexander-bachmann@gmx.de>
2024-03-09 20:13:31 +0100

    Add tailwindcss-animated

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -15,7 +15,9 @@ const Dropdown: React.FC<DropdownProps> = ({ title, items, isOpen, onToggle}) =>
             </div>
             <div>
             { isOpen && (
-                    <div className="origin-top-right absolute right-0 mt-2 rounded-md shadow-lg bg-gray-500 ring-1 ring-black ring-opacity-5">
+                    <div className="origin-top-right absolute right-0 mt-2 rounded-md 
+                    shadow-lg bg-gray-500 ring-1 ring-black ring-opacity-5
+                    animate-fade-down animate-once animate-ease-linear">
                         <div className="mx-3 flex flex-col text-right">
                             {items.map((item, key) =>(
                                 <a className="py-1" key={key} href="#">{item}</a>
