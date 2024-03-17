a502303f4840876cbf785daa09313edb33989405
Alexander Bachmann <alexander-bachmann@gmx.de>
2024-03-17 21:42:52 +0100

    more design fixes

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ content }) => {
   return (
-    <div className="flex flex-row text-gray-800">
+    <div className="flex flex-row h-screen text-gray-800">
       <div className="fixed inset-0 h-full w-full bg-stadiumTop bg-cover bg-center z-0" />
       <div className="fixed inset-0 h-full w-full bg-black bg-opacity-50 z-10" />
       <main className="w-full flex-grow flex items-center justify-center z-20">
--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -46,8 +46,8 @@ const Sidebar: React.FC<SidebarProps> = ({ color }) => {
 
   return (
     <div className="flex flex-row w-auto h-full text-gray-200 items-center lg:flex-col lg:h-screen lg:items-start">
-      <div className=" hidden lg:block items-center justify-center h-14 border-b border-gray-700 lg:w-full lg:justify-start lg:border-b-0 lg:pl-4">
-        <h1 className="text-xl font-bold">BBet</h1>
+      <div className=" hidden lg:block items-center justify-center h-10 border-b border-gray-700 lg:w-full lg:justify-start lg:border-b-0 lg:pl-4 lg:mg-3">
+        <h1 className="text-xl font-bold text-center">BBet</h1>
       </div>
       <ul className="flex flex-row flex-wrap w-full h-full pt-3 px-2 space-x-1 justify-center items-center lg:flex-col lg:justify-start lg:pr-0 lg:space-x-0 lg:space-y-3">
         {sidebarItems.map((item) => (
