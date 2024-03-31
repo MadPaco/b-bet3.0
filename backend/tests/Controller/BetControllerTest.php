bcb08763e3593ebfb4ce75626c062f35394b989c
Alexander Bachmann <alexander-bachmann@gmx.de>
2024-03-31 19:54:29 +0200

    add basic BetController tests

--- backend/tests/Controller/BetControllerTest.php
+++ backend/tests/Controller/BetControllerTest.php
@@ -6,10 +6,54 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BetControllerTest extends WebTestCase
 {
+
+    private $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+        $this->logIn();
+    }
+
+    private function logIn()
+    {
+
+        $this->client->request(
+            'POST',
+            '/api/register',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'email' => 'test@example.com',
+                'password' => 'password',
+                'username' => 'testuser',
+                'favTeam' => 'Arizona Cardinals'
+            ])
+        );
+
+        $this->client->request(
+            'POST',
+            '/api/login_check',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'username' => 'testuser',
+                'password' => 'password',
+            ])
+        );
+
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+    }
+
     public function testAddBet(): void
     {
-        $client = static::createClient();
-        $client->request(
+        $this->client->request(
             'POST',
             '/api/bet/addBets',
             [],
@@ -24,17 +68,14 @@ class BetControllerTest extends WebTestCase
             ])
         );
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertJson($client->getResponse()->getContent());
-        $this->assertContains('Bet added or updated!', $client->getResponse()->getContent());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
     public function testFetchBets(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/api/bet/fetchBets?user=testuser&weekNumber=1');
+        $this->client->request('GET', '/api/bet/fetchBets?user=testuser&weekNumber=1');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertJson($client->getResponse()->getContent());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertJson($this->client->getResponse()->getContent());
     }
 }
